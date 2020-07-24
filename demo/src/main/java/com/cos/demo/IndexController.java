3cbc22098972d8024baccfd016a4adf743e166dc
kdm <cokj1610@naver.com>
2020-07-24 14:32:11 +0900

    인터셉터, exception , 글쓰기

--- demo/src/main/java/com/cos/demo/IndexController.java
+++ demo/src/main/java/com/cos/demo/IndexController.java
@@ -13,6 +13,11 @@ public class IndexController {
 	@Autowired
 	Test t;
 
+	@GetMapping("/index")
+	public String home() {
+		return "index";
+	}
+		
 	// http://localhost:8080/
 	@GetMapping({ "", "/" })
 	public @ResponseBody String index() {
